a0d1cde2e515a6ada06e20578c957f182d800f46
DSWallach <dawallach@vassar.edu>
2017-05-02 08:38:14 -0400

    Cleaning up

--- src/nn-go.lisp
+++ src/nn-go.lisp
@@ -12,6 +12,7 @@
   (:export nn-kernel)
   (:export vec-add-kernel)
   (:export :main)
+  (:export :main-add)
   (:export :tester)
   (:export mat1)
   (:export mat2))
@@ -67,13 +68,7 @@
                              (aref B k j)))))))
     C))
 
-(defkernel vec-add-kernel (void ((a float*) (b float*) (c float*) (n int)))
-  (let ((i (+ (* block-dim-x block-idx-x) thread-idx-x)))
-    (if (< i n)
-      (set (aref c i)
-           (+ (aref a i) (aref b i))))))
 
-;; Example from cl-cuda github
 (defkernel nn-kernel (void ((a float*) (b float*) (c float*) (m int) (n int)))
            (let ((i (+ (* block-dim-x block-idx-x) thread-idx-x))
                  (j (+ (* block-dim-y block-idx-y) thread-idx-y))
@@ -91,6 +86,46 @@
 
 (defun tester () 
   (verify-mat-mul (make-matrix 2048 2048) (make-matrix 2048 2048)))
+
+(defun random-init (data n)
+  (dotimes (i n)
+    (setf (memory-block-aref data i) (random 1.0))))
+
+(defun verify-result (as bs cs n)
+  (dotimes (i n)
+    (let ((a (memory-block-aref as i))
+          (b (memory-block-aref bs i))
+          (c (memory-block-aref cs i)))
+      (let ((sum (+ a b)))
+        (when (> (abs (- c sum)) 1.0)
+          (error (format nil "verification fault, i:~A a:~A b:~A c:~A"
+                         i a b c))))))
+  (format t "verification succeed.~%"))
+
+(defkernel vec-add-kernel (void ((a float*) (b float*) (c float*) (n int)))
+  (let ((i (+ (* block-dim-x block-idx-x) thread-idx-x)))
+    (if (< i n)
+        (set (aref c i)
+             (+ (aref a i) (aref b i))))))
+
+(defun main-add ()
+  (let* ((dev-id 0)
+         (n 4096)
+         (threads-per-block 256)
+         (blocks-per-grid (/ n threads-per-block)))
+    (with-cuda (dev-id)
+      (with-memory-blocks ((a 'float n)
+                           (b 'float n)
+                           (c 'float n))
+        (random-init a n)
+        (random-init b n)
+        (sync-memory-block a :host-to-device)
+        (sync-memory-block b :host-to-device)
+        (vec-add-kernel a b c n
+                        :grid-dim (list blocks-per-grid 1 1)
+                        :block-dim (list threads-per-block 1 1))
+        (sync-memory-block c :device-to-host)
+        (verify-result a b c n)))))
 
 (defun main ()
   (let* ((dev-id 0)
